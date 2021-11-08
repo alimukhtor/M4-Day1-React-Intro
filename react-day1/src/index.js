b142861b2d07c128f7b6f4ff8d8a9262e4e62b99
alimukhtor <james1999007@gmail.com>
2021-11-08 18:24:51 +0100

    imported jsx file

--- react-day1/src/index.js
+++ react-day1/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Counter from './components/Counter'
+
+
 ReactDOM.render(<App />, document.getElementById('root')
 );
